cec3e90f978141c1f591102075706b771494c077
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-01-10 10:24:22 +0000

    Reviewed implementation of the HttpViewModel trait

--- src/Traits/HttpViewModel.php
+++ src/Traits/HttpViewModel.php
@@ -5,40 +5,54 @@ namespace Drewlabs\Packages\Http\Traits;
 use Drewlabs\Core\Validator\Traits\ViewModel;
 use Illuminate\Http\Request;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Container\ContainerInterface;
 
 trait HttpViewModel
 {
     use ViewModel;
+    use HasIocContainer;
 
-    private function __construct()
-    {
-    }
-
-    public function fromIlluminateHttpRequest(Request $request)
-    {
-        return $this->setUser($request->user())
-            ->files($request->allFiles())
-            ->withBody($request->all());
-    }
-
-    public function fromPsrServerRequest(ServerRequestInterface $request)
-    {
-        return $this->withBody(
-            array_merge(
-                (array)($request->getParsedBody() ?? []),
-                $request->getQueryParams()
-            )
-        );
-    }
-
-    public static function create($request)
+    /**
+     * 
+     * @param ServerRequestInterface|Request|mixed $request 
+     * @return self 
+     */
+    public function __construct($request)
     {
         if ($request instanceof ServerRequestInterface) {
-            return (new self)->fromPsrServerRequest($request);
+            $this->fromPsrServerRequest($request);
+        } else if ($request instanceof Request) {
+            $this->fromLaravelRequest($request);
         }
-        if ($request instanceof Request) {
-            return (new self)->fromIlluminateHttpRequest($request);
-        }
-        return new self;
+    }
+
+    /**
+     * Creates an instance of the viewModel class
+     * 
+     * @param ServerRequestInterface|Request|mixed $request 
+     * @param ContainerInterface|\ArrayAccess|null $container 
+     * @return HttpViewModel 
+     */
+    public static function create($request = null, $container = null)
+    {
+        $request = $request ?: self::createResolver(Request::class)($container);
+        return new self($request);
+    }
+
+    protected function fromLaravelRequest(Request $request)
+    {
+        return $this->setUser($request->user())
+            ->withBody($request->all())
+            ->files($request->allFiles());
+    }
+
+    protected function fromPsrServerRequest(ServerRequestInterface $request)
+    {
+        $body = array_merge(
+            $request->getQueryParams() ?: [],
+            (array)($request->getParsedBody() ?? [])
+        );
+        return $this->withBody($body)
+            ->files($request->getUploadedFiles());
     }
 }

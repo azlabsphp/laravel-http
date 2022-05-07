9252dda4cced16fd2371b87aa172ddabb44f8b1e
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-05-07 18:27:32 +0000

    Added validated method to HttpViewModel::trait to allow view model to be able to validates request internally when called

--- src/Traits/HttpViewModel.php
+++ src/Traits/HttpViewModel.php
@@ -2,14 +2,19 @@
 
 namespace Drewlabs\Packages\Http\Traits;
 
+use Closure;
+use Drewlabs\Contracts\Validator\Validator;
+use Drewlabs\Core\Helpers\Functional;
 use Illuminate\Http\Request;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Container\ContainerInterface;
+use RuntimeException;
 
 trait HttpViewModel
 {
     use \Drewlabs\Core\Validator\Traits\ViewModel;
     use ContainerAware;
+
 
     /**
      * 
@@ -82,4 +87,87 @@ trait HttpViewModel
         }
         return $this->request;
     }
+
+    //#region Miscellanous
+    /**
+     * Validates the view model object using the bounded validator {@see Validator} instance.
+     * 
+     * if the view model provides updateRules(), passing $updating=true will load the update
+     * rules
+     * 
+     * ```php
+     * <?php
+     * $viewmodel = new ViewModelClass($request);
+     * 
+     * // Validating
+     * $viewmodel = $viewmodel->validated();
+     * 
+     * // To execute a method after validating the model
+     * $viewmodel->validated(function() use ($viewmodel) {
+     *  // Persist data to database after validation
+     * });
+     * 
+     * // In order to use update rules
+     * // This will throw an exception if the validation fails
+     * $viewmodel->validated(true);
+     * 
+     * // In order to use the update rules and pass a callback
+     * // which runs when validation passes
+     * $viewmodel->validated(true, function() use ($viewmodel) {
+     *  // Persist data to database after validation
+     * });
+     * ```
+     * 
+     * @param Closure $callback
+     * @throws \Drewlabs\Core\Validator\Exceptions\ValidationException
+     * @return self 
+     */
+    public function validated(...$args)
+    {
+        $argCount = count($args);
+        if ($argCount === 1 && Functional::isCallable($args[0])) {
+            return $this->validateWithCallback($this->createValidator(), $args[0]);
+        } else if ($argCount === 1 && boolval($args[0]) === true) {
+            return $this->validate($this->createValidator(true));
+        } else if ($argCount > 1) {
+            return $this->validate($this->createValidator(true), $args[1]);
+        } else {
+            return $this->validate($this->createValidator());
+        }
+    }
+
+    /**
+     * @return Validator 
+     */
+    private function createValidator(bool $updating = false)
+    {
+        /**
+         * @var Validator
+         */
+        $validator = self::createResolver(Validator::class)();
+        return $updating ? $validator->updating() : $validator;
+    }
+
+    private function validateWithCallback(Validator $validator, \Closure $callback)
+    {
+        return $validator->validate($this, $callback);
+    }
+
+    private function validate(Validator $validator)
+    {
+        $validator = $validator->validate($this);
+        if ($validator->fails()) {
+            $deprecatedException = \Drewlabs\Core\Validator\Exceptions\ValidationException::class;
+            $exception = \Drewlabs\Validator\Exceptions\ValidationException::class;
+            if (class_exists($deprecatedException)) {
+                throw new $deprecatedException($validator->errors());
+            } else if (class_exists($exception)) {
+                throw new $exception($validator->errors());
+            } else {
+                throw new RuntimeException('Failed validating view model instance');
+            }
+        }
+        return $this;
+    }
+    //#endregion
 }
